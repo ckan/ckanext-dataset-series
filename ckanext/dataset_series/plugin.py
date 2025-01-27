5cecdad9fbec52972dd0f31e5813df5a84f3fe48
amercader <amercadero@gmail.com>
2025-01-27 14:00:42 +0100

    Add example schemas

--- ckanext/dataset_series/plugin.py
+++ ckanext/dataset_series/plugin.py
@@ -1,9 +1,15 @@
-import ckan.plugins as plugins
+import json
+
+import ckan.plugins as p
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.dataset_series.actions import package_show
 
-class DatasetSeriesPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+
+class DatasetSeriesPlugin(p.SingletonPlugin):
+    p.implements(p.IConfigurer)
+    p.implements(p.IActions)
+    p.implements(p.IPackageController, inherit=True)
 
     # IConfigurer
 
@@ -11,3 +17,19 @@ class DatasetSeriesPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "dataset_series")
+
+    # IActions
+    def get_actions(self):
+        return {
+            "package_show": package_show,
+        }
+
+    # IPackageController
+
+    def before_dataset_index(self, dataset_dict):
+        if dataset_dict.get("in_series"):
+            try:
+                dataset_dict["vocab_in_series"] = json.loads(dataset_dict["in_series"])
+            except ValueError:
+                pass
+        return dataset_dict
